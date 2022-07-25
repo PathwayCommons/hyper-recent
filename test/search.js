563618515d2461ce1f76191a609dd6fc7977dc16
Jeffrey Wong <jvwong@gmail.com>
2022-07-25 14:55:04 -0400

    - exact, simple phrase test - differentiate between root and

--- test/search.js
+++ test/search.js
@@ -7,16 +7,14 @@ import { Search } from '../src/search.js';
 const normalizedRangeData = fs.readFileSync('test/data/normalized_range_20220622.json');
 
 describe('search module', function () {
-  let searcher = new Search();
-  describe('strict keywords', function () {
-    const strictSearch = async queryString => searcher.search(queryString, { combineWith: 'AND' });
+  const searcher = new Search();
+  const strictSearch = async queryString => searcher.search(queryString, { combineWith: 'AND' });
 
-    before(async function () {
-      const collection = JSON.parse(normalizedRangeData);
-      searcher = new Search();
-      await searcher.articles(collection);
-    });
-
+  before(async function () {
+    const collection = JSON.parse(normalizedRangeData);
+    await searcher.articles(collection);
+  });
+  describe('exact keywords', function () {
     it('returns expected articles for single keyword', async () => {
       const term = 'cognitive';
       const expectedDois = [
@@ -31,6 +29,51 @@ describe('search module', function () {
       ];
 
       const hits = await strictSearch(term);
+      const dois = hits.map(hit => hit.id);
+      const common = _.intersection(dois, expectedDois);
+      expect(common).to.have.lengthOf(expectedDois.length);
+    });
+
+    it('returns expected articles for two keywords', async () => {
+      const term = 'cancer patient';
+      const expectedDois = [
+        '10.1101/2022.03.29.486212',
+        '10.1101/2022.06.20.496910'
+      ];
+
+      const hits = await strictSearch(term);
+      const dois = hits.map(hit => hit.id);
+      const common = _.intersection(dois, expectedDois);
+      expect(common).to.have.lengthOf(expectedDois.length);
+    });
+  });
+
+  describe('root keyword of plural', function () {
+    it('returns expected articles for two roots', async () => {
+      const term = 'cancer patient';
+      const expectedDois = [
+        '10.1101/2022.03.29.486212',
+        '10.1101/2022.06.05.494848',
+        '10.1101/2022.06.21.496924',
+        '10.1101/2022.06.20.496910'
+      ];
+
+      const hits = await strictSearch(term);
+      const dois = hits.map(hit => hit.id);
+      const common = _.intersection(dois, expectedDois);
+      expect(common).to.have.lengthOf(expectedDois.length);
+    });
+  });
+
+  describe('short phrase', function () {
+    it('returns expected articles for single phrase', async () => {
+      const phrase = '"gene regulation"';
+      const expectedDois = [
+        '10.1101/2022.06.21.497104',
+        '10.1101/2022.06.20.496844'
+      ];
+
+      const hits = await strictSearch(phrase);
       const dois = hits.map(hit => hit.id);
       const common = _.intersection(dois, expectedDois);
       expect(common).to.have.lengthOf(expectedDois.length);
