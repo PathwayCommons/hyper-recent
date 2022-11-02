e23ea6cfd4935873dafe87d92f54d4ed882cd60a
Jeffrey Wong <jvwong@gmail.com>
2022-11-02 09:54:03 -0400

    Sample data for alzheimers cat

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CATEGORY_ID="alzheimer-disease"
+CATEGORY_ID="alzheimers-disease"
 DATA_DIRECTORY="../example-data"
 
 MEDRXIV_SOURCE="medrxiv"
@@ -21,8 +21,22 @@ echo "numPapersRawMedrxiv: ${numPapersRawMedrxiv}"
 
 echo "Combining results..."
 combined=$(jq -s add ${DATA_DIRECTORY}/${DATE_PREFIX}_*.json)
+rm "${DATA_DIRECTORY}/${DATE_PREFIX}_${BIORXIV_SOURCE}.json"
+rm "${DATA_DIRECTORY}/${DATE_PREFIX}_${MEDRXIV_SOURCE}.json"
+
+DATA_FILE="${DATA_DIRECTORY}/2022-10.json"
+echo ${combined} | jq >> ${DATA_FILE}
+
+
+QUERY="alzheimer"
+OUTPUT_FILE="${DATA_DIRECTORY}/${CATEGORY_ID}.json"
+echo "Searching for ${CATEGORY_ID}"
+searchHits=$(node ../src/cli.js search --strict --input=${DATA_FILE} ${QUERY})
+numSearchHits=$(echo ${searchHits} | jq 'length')
+echo "Found ${numSearchHits} hits"
+
 collection=$(
-    echo ${combined} | jq '[.[] |
+    echo ${searchHits} | jq '[.[] |
     {
       paperId: .doi,
       doi: .doi,
@@ -34,7 +48,5 @@ collection=$(
     }]'
   )
 
-echo ${collection} | jq >> "${DATA_DIRECTORY}/2022-10.json"
+echo ${collection} | jq >> ${OUTPUT_FILE}
 
-rm "${DATA_DIRECTORY}/${DATE_PREFIX}_${BIORXIV_SOURCE}.json"
-rm "${DATA_DIRECTORY}/${DATE_PREFIX}_${MEDRXIV_SOURCE}.json"
