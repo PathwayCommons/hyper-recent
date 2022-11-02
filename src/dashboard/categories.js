1b7ae17e3e0dfb94c0d1989087964ee3aec14af5
Jeffrey Wong <jvwong@gmail.com>
2022-11-02 11:39:00 -0400

    Topics in alzheimers: neuroinflammation and therapeutics

--- src/dashboard/categories.js
+++ src/dashboard/categories.js
@@ -1,8 +1,12 @@
 import alzheimersDisease from '../../example-data/alzheimers-disease.json';
 import alzheimersDiagnosis from '../../example-data/alzheimers-diagnosis.json';
+import alzheimersNeuroinflammation from '../../example-data/alzheimers-neuroinflammation.json';
+import alzheimersTherapeutics from '../../example-data/alzheimers-therapeutics.json';
 
 const ALZHEIMERS_DISEASE_ID = 'alzheimers-disease';
 const ALZHEIMERS_DIAGNOSIS_ID = 'alzheimers-diagnosis';
+const ALZHEIMERS_NEUROINFLAMMATION_ID = 'alzheimers-neuroinflammation';
+const ALZHEIMERS_THERAPEUTICS = 'alzheimers-therapeutics';
 
 const toDate = o => {
   o.date = new Date(o.date);
@@ -18,7 +22,17 @@ export const categories = [
   {
     id: ALZHEIMERS_DIAGNOSIS_ID,
     name: 'Alzheimer\'s Disease: Diagnosis',
-    description: 'Diagnostics, biomarkers and phenotypes related to the progression of AD.'
+    description: 'Diagnostics, biomarkers and their relationship to AD phenotypes.'
+  },
+  {
+    id: ALZHEIMERS_NEUROINFLAMMATION_ID,
+    name: 'Alzheimer\'s Disease: Neuroinflammation',
+    description: 'Relationships between neuroinflammation and amyloid and tau pathologies and its effect on disease trajectory.'
+  },
+  {
+    id: ALZHEIMERS_THERAPEUTICS,
+    name: 'Alzheimer\'s Disease: Therapeutics',
+    description: 'Nonpharmacological and pharmacological management of the cognitive and behavioral symptoms of AD.'
   }
 ];
 
@@ -31,6 +45,12 @@ export const getPapers = ({ id, limit = 20 }) => {
       break;
     case ALZHEIMERS_DIAGNOSIS_ID:
       papers = alzheimersDiagnosis;
+      break;
+    case ALZHEIMERS_NEUROINFLAMMATION_ID:
+      papers = alzheimersNeuroinflammation;
+      break;
+    case ALZHEIMERS_THERAPEUTICS:
+      papers = alzheimersTherapeutics;
       break;
     default:
       console.error(`Unable to load ${id}.`);
