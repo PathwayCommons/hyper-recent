3179ef126af2d379223ba297dbb7fbbf3d5e3f02
Suhyma <suhyma02@gmail.com>
2023-02-10 16:06:35 -0500

    environment / script improvements

--- src/data-search.js
+++ src/data-search.js
@@ -3,6 +3,7 @@
 // here we will convert the search.sh script into JS
 import { format, sub } from 'date-fns';
 import fs from 'fs';
+import { options } from 'preact';
 import { download, search } from './cli.js';
 
 const CATEGORY_ID = 'alzheimers-disease';
@@ -17,8 +18,20 @@ const START_DATE = format(sub(now, startOffset), 'yyyy-MM-dd');
 const END_DATE = format(now, 'yyyy-MM-dd');
 
 console.log(`Fetching from ${BIORXIV_SOURCE} between ${START_DATE} and ${END_DATE}`);
-download(START_DATE, END_DATE, BIORXIV_SOURCE);
+download(START_DATE, END_DATE, BIORXIV_SOURCE); // not sure if the 3rd param is correct here
+// is just calling the download function enough? do we add something to account for the --output part in the script
+
+console.log(`Fetching from ${MEDRXIV_SOURCE} between ${START_DATE} and ${END_DATE}`);
+download(START_DATE, END_DATE, MEDRXIV_SOURCE); // not sure if the 3rd param is correct here
+// is just calling the download function enough? do we add something to account for the --output part in the script
 
 console.log('Combining results...');
 const DATA_FILE = `${DATA_DIRECTORY}/${END_DATE}.json`;
-const dataFile = fs.openSync(DATA_FILE, 'w');
+const dataFile = fs.openSync(DATA_FILE, 'w'); // unsure about this solution as well to create a new file
+
+const QUERY = 'alzheimer';
+const OUTPUT_FILE = `${DATA_DIRECTORY}/${CATEGORY_ID}.json`;
+console.log(`Searching for ${QUERY}`);
+const searchHits = search(QUERY, true); // also unsure about param 2
+const numSearchHits = Object.keys(searchHits).length;
+console.log(`Found ${numSearchHits} hits`);
