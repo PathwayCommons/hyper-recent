1a120bf6b4382e9e87723db57dc997425dab3e96
Suhyma <suhyma02@gmail.com>
2023-03-08 10:35:05 -0500

    Cleaning up data-search.js

--- src/data-search.js
+++ src/data-search.js
@@ -11,17 +11,16 @@ import { writeFormattedJSON } from './cli.js';
  * @returns {collection}, a JSON array of search results for each set topic.
  */
 export async function getData () {
-  // set dates for past month
-
+  // Set dates for past month
   const now = new Date();
-  const startOffset = { days: 1 };
+  const startOffset = { months: 1 };
   const start = format(sub(now, startOffset), 'yyyy-MM-dd');
   const end = format(now, 'yyyy-MM-dd');
 
-  // reading config file for list of topics
+  // Reading config file for list of topics
   const config = JSON.parse(fs.readFileSync('example-data/data-config.json'));
 
-  // download all recent papers & combine the arrays
+  // Download all recent papers & combine the arrays
   const data = await Promise.all([
     download('biorxiv', start, end),
     download('medrxiv', start, end)
@@ -29,7 +28,7 @@ export async function getData () {
 
   const articles = _.flatten(data);
 
-  // search using list of topic objects from config
+  // Search using list of topic objects from config
   const searcher = new Search();
 
   await searcher.articles(articles);
@@ -41,16 +40,13 @@ export async function getData () {
     return _.assign({}, config, { papers });
   };
   const collection = await Promise.all(config.map(doSearches));
-  // const parsedCollection = JSON.parse(collection);
 
-  // output all search result papers into data json
+  // Output all search result papers into data.json
   const fileHandle = await fs.openSync('example-data/data.json', 'w');
   await writeFormattedJSON(collection, 'example-data/data.json');
   fs.closeSync(fileHandle);
 
-  return collection;
+  // return collection;
 }
 
-const out = await getData();
-console.log(out[0].papers.length);
-console.log('done!');
+await getData();
