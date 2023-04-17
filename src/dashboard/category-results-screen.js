cb4341995f7481ebdd9467dd76f3b47982b22947
Suhyma <suhyma02@gmail.com>
2023-04-17 16:31:42 -0400

    Adding initial affiliation tooltip

--- src/dashboard/category-results-screen.js
+++ src/dashboard/category-results-screen.js
@@ -9,7 +9,9 @@ function Paper ({ paper }) {
       h('div', { class: 'paper-read' }),
       h('div', { class: 'paper-title' }, paper.title)
     ]),
-    h('div', { class: 'paper-authors' }, paper.authors),
+    h('div', { class: 'paper-authors tooltip' }, paper.authors, [
+      h('span', { class: 'tooltiptext' }, paper.author_corresponding_institution)
+    ]),
     h('div', { class: 'paper-date' }, paper.date.toISOString().split('T')[0]),
     h('div', { class: 'paper-journal' }, paper.journal),
     h('div', { class: 'paper-brief' }, paper.brief)
